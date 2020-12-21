f97480a95812a62324f43696c856a23347a1122a
Payam Khaninejad <khaninejad@gmail.com>
2020-12-21 23:39:40 +0330

    Hello World

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Payam on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1fbe9e5c5c83d976db75d1445b3118a12bb6ad7b
Payam Khaninejad <khaninejad@gmail.com>
2020-12-22 21:04:51 +0330

    rename folder

---
+++ 4.Hiking/Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Payam on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

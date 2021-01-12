415f1a53cebaaf70e3f1b8e805a0fb6ae798252e
Payam Khaninejad <khaninejad@gmail.com>
2021-01-13 00:13:27 +0330

    basic gesture

---
+++ BasicGesture/BasicGesture/BasicGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicGestureApp.swift
+//  BasicGesture
+//
+//  Created by Payam on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct BasicGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

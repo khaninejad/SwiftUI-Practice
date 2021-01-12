350d8ef7cdfbaf88fc0c5fd11101035395ca5fd4
Payam Khaninejad <khaninejad@gmail.com>
2021-01-13 00:31:10 +0330

    drag gesture

--- 24. BasicGesture/BasicGesture/ContentView.swift
+++ 24. BasicGesture/BasicGesture/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tapped : Bool = false
+    @State private var onDragChanged = CGSize.zero
     var body: some View {
         Card(tapped: self.tapped)
+            .offset(y: self.onDragChanged.height)
+            .gesture(DragGesture()
+                        .onChanged { value in
+                            onDragChanged = value.translation
+                        }
+                        .onEnded{ value in
+                            onDragChanged = CGSize.zero
+                        }
+            )
             .gesture(TapGesture(count: 1)
                         .onEnded({ () in
                             self.tapped.toggle()

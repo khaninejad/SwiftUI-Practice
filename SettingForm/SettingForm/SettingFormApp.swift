6dbf730bf0b8cd52441a96876d1e6c6d12195bf7
Payam Khaninejad <khaninejad@gmail.com>
2021-01-14 00:40:48 +0330

    simple settings form

---
+++ SettingForm/SettingForm/SettingFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingFormApp.swift
+//  SettingForm
+//
+//  Created by Payam on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SettingFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

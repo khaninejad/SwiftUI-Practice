90fc65cec97ae8b97ca8e92a6680f9441d2803a9
Payam Khaninejad <khaninejad@gmail.com>
2021-01-14 00:40:56 +0330

    simple settings form

--- SettingForm/SettingForm/ContentView.swift
+++ SettingForm/SettingForm/ContentView.swift
@@ -8,7 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isOnButton: Bool = false
+    @State private var optionTwo: Bool = false
+    @State private var sliderOption: CGFloat = CGFloat.zero
+    
     var body: some View {
+        NavigationView {
+            Form{
+                
+                Section(header: Text("This is a header Text This is a header Text This is a header Text This is a header Text This is a header Text This is a header Text").padding(5).lineLimit(nil), content: {
+                    Toggle(isOn: self.$isOnButton, label: {
+                        Text("Scheduled")
+                    })
+                })
+                
+                
+                HStack {
+                    
+                    VStack {
+                        Text("From")
+                        Text("To")
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: Text("Destination")){
+                        VStack {
+                            Text("Sunset").foregroundColor(.blue)
+                            Text("Sunrise").foregroundColor(.blue)
+                        }
+                    }.fixedSize()
+                    
+                    
+                    
+                }
+                
+                Section(header: Text(""), content: {
+                    Toggle(isOn: self.$optionTwo, label: {
+                        Text("Option Two ")
+                    })
+                    
+                })
+                
+                Section(header: Text(""), content: {
+                    
+                    HStack{
+                        Text("Left Option")
+                        Slider(value: $sliderOption)
+                        Text("Right Option")
+                    }
+                    
+                })
+                    
+                
+                
+            }
+            
+        }
+        
         Text("Hello, world!")
             .padding()
     }

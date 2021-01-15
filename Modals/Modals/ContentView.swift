1660dc87ad3f5afea4c1293876a4665e7498eeeb
Payam Khaninejad <khaninejad@gmail.com>
2021-01-15 13:43:38 +0330

    modals

---
+++ Modals/Modals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modals
+//
+//  Created by Payam on 1/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Modals/Modals/ModalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalsApp.swift
+//  Modals
+//
+//  Created by Payam on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ModalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
